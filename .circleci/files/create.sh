22ddfa2d68bef9a65fcb56a0cfe76495536240f4
ahmaddfathyy <kabayly.am@gmail.com>
2022-05-12 05:10:20 +0200

    Cloud Front Update

--- .circleci/files/create.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-aws cloudformation deploy \
---template-file cloudfront.yml \
---stack-name InitialStack \
---parameter-overrides WorkflowID=ku9901wil6iuv8u \
---region us-west-2 \
---profile mkhalil
